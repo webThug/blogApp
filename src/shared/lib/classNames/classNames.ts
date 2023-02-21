531d7c533e2d1b64267c36ceb8ac844fa30a8712
Иван <lostnemo@rambler.ru>
2023-02-21 23:36:21 +0600

    add config

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,11 +1,11 @@
 
 type Mods = Record<string, string | boolean>
 
-export function classNames(cls: string, mods: Mods, aditional: string[]): string {
+export function classNames(cls: string, mods: Mods ={}, additional: string[] = []): string {
 
     return [
         cls,
-        ...aditional,
+        ...additional.filter(Boolean),
         ...Object.entries(mods)
             .filter(([className, value]) => Boolean(value))
             .map(([className])=> className)
