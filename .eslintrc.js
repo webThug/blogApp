1ecafc7ff53242be6203a4856731bf814f817a7d
Иван <lostnemo@rambler.ru>
2023-03-07 20:17:16 +0300

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,6 +50,7 @@ module.exports = {
         'jsx-a11y/click-events-have-key-events': 'off',
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'error',
+        'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
     },
     globals: {
         __IS_DEV__: true,
